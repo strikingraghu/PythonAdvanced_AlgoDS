fbbfdd93eaa3e8a496aa6623e550061602872dfa
Raghavendra Guttur <strikingraghu@gmail.com>
2019-08-06 14:41:59 +0530

    PythonLab

--- Variables_Constants_Literals.py
+++ Variables_Constants_Literals.py
@@ -1,8 +1,35 @@
 """
 
+Variable -
 A variable is a named location used to store data in the memory.
 It is helpful to think of variables as a container that holds data which can be changed later throughout programming.
 We don't assign values to the variables, whereas Python gives the reference of the object (value) to the variable.
+
+Constants -
+A constant is a type of variable whose value cannot be changed.
+It is helpful to think of constants as containers that hold information which cannot be changed later.
+Ex - You can think of constant as a bag to store some books and those books cannot be replaced once placed in the bag.
+
+Literals -
+Literal is a raw data given in a variable or constant. In Python, there are various types of literals:
+
+Numeric Literals
+    Numeric Literals are immutable (unchangeable).
+    Numeric literals can belong to 3 different numerical types Integer, Float and Complex.
+
+String Literals
+    A string literal is a sequence of characters surrounded by quotes.
+    We can use both single, double or triple quotes for a string.
+    And, a character literal is a single character surrounded by single or double quotes.
+
+Boolean Literals
+    A Boolean literal can have any of the two values: True or False.
+
+Special Literals (None)
+    Python contains one special literal i.e. None. We use it to specify to that field that is not created.
+
+Literal Collections -
+There are four different literal collections List literals, Tuple literals, Dict literals, and Set literals.
 
 """
 
@@ -36,4 +63,33 @@ print()
 # strings
 string_1 = "Hello, are you part of Bengaluru city?"
 string_2 = "Indeed, it's a great city!"
+
+# constants (Try to create a constant.py and import this module any python program like constant.PI)
+PI = 3.14
+GRAVITY = 9.8
+
+# numeric literals
+
+a = 0b1010
+b = 100
+c = 0o446
+d = 0x19b
+print('A =', a, '|', 'B =', b, '|', 'C =', c, '|', 'D =', d)
+
+float_1 = 10.5
+float_2 = 1.5e2
+print('Float 1 =', float_1, '|', 'Float 2 =', float_2)
+
+complex_x = 3.14j
+print('X value is =', complex_x, '|', 'Type is =', type(complex_x))
+
+# string literals
+strings = 'This is a Python code'
+char = 'R'
+multiple_line_str = """This is a multi-line comment"""
+raw_string = r"raw \n string"
+print(strings, '|', char, '|', multiple_line_str, '|', raw_string)
+
+
+
 
