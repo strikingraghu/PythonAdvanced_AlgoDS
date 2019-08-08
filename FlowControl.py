662a3662935e28c9e5940a9d677c029b00bcf36c
Raghavendra Guttur <strikingraghu@gmail.com>
2019-08-08 19:05:34 +0530

    PythonLab

---
+++ FlowControl.py
@@ -0,0 +1,21 @@
+"""
+    Decision making is anticipation of conditions occurring while execution of the program and specifying actions
+    taken according to the conditions. Decision structures evaluate multiple expressions which produce TRUE or FALSE as
+    an outcome. You need to determine which action to take and which statements to execute if outcome is TRUE or FALSE!
+    Types of Decision Making logic -
+        Python IF ELSE Statement
+        Python IF...ELIF...ELSE Statements
+        Python nested IF statements
+"""
+
+# if condition
+
+num = 3
+if num > 0:
+    print(num, "is a positive number!")
+print("This is always printed")
+
+num = -1
+if num > 0:
+    print(num, "is a positive number")
+print("This is also always printed")
--- Input_Output.py
+++ Input_Output.py
@@ -13,6 +13,7 @@ double_check_with_user = (input("Do you want to enter another number? "))
 total_sum = 0
 give_another_num = 0
 total_sum += give_num
+
 while double_check_with_user != 'no':
     give_another_num = int(input('Ok, type another number you like in this console! '))
     if type(give_another_num) == int:
@@ -21,3 +22,14 @@ while double_check_with_user != 'no':
         if check_again == 'no':
             print(total_sum)
             break
+
+# output section
+# (curly braces {} are used as placeholders)
+x = 5
+y = 10
+print("The value of variable x = {} and value of variable y = {}".format(x, y))
+
+# different ways to dump values within placeholders
+print("{} city is better than {}".format('Bangalore', 'Bhopal'))
+print("{0} city is better than {1}".format('Bangalore', 'Bhopal'))
+print("{1} city is better than {0}".format('Bangalore', 'Bhopal'))
