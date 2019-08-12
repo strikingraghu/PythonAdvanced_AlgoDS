0e7d876252ba84f039be855b5d134a52403c08f3
Raghavendra Guttur <strikingraghu@gmail.com>
2019-08-12 16:01:47 +0530

    PythonLab

--- Advanced_Datatypes.py
+++ Advanced_Datatypes.py
@@ -34,3 +34,25 @@ print(math.factorial(6))
 
 var_x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 print("Random Output :", random.choice(var_x))
+print('-----------------------')
+
+# lists
+sample_list1 = ["Happy", [2, 0, 1, 5]]
+print("List Output :", sample_list1)
+print("List Output :", sample_list1.__len__())
+print("List Output :", sample_list1[1][3])  # Iterating on 2nd element within this list and picking index [3] element
+
+sample_list2 = [2, 4, 98, 283, 192, 99, 13, 587, 539, 987]
+print("Negative Indexing List Output :", sample_list2[-4])
+print("List Output :", sample_list2[1:4])
+sample_list2.extend([1000, 2000, 3000])
+print("Extended List Output :", sample_list2)
+del sample_list2[4:5]
+print("After Deleting Element List Output :", sample_list2)
+sample_list2.pop()
+print("After Pop() Call :", sample_list2)
+print('-----------------------')
+
+
+# tuples
+
