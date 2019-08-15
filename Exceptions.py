4cf42c64d8993da4197ee4b8bfa8a1ff93d405d6
Raghavendra Guttur <strikingraghu@gmail.com>
2019-08-15 20:20:30 +0530

    PythonLab

---
+++ Exceptions.py
@@ -0,0 +1,52 @@
+"""
+
+Python provides two very important features to handle any unexpected error in your Python programs
+and to add debugging capabilities! Exception Handling − This would be covered in this script.
+    a) Exception Handling/Standard Exceptions.
+    b) Assertions.
+
+"""
+import sys
+
+
+random_list = ['a', 'e', 'i', 'o', 'u', 1, 2, 3, 4, 5]
+for each_element in random_list:
+    try:
+        print("The element is :", each_element)
+        if each_element % 2 == 0:
+            print("Element divisible by 2")
+        else:
+            print("Not an integer or numeric value not divisible by 2")
+    except TypeError:
+        print("Oops!", sys.exc_info()[0], "occured.")
+        print("Next Entry")
+        print()
+print("Done with all steps!")
+print('-----------------------')
+
+# io error
+
+try:
+    file_handling = open("txt")  # To test this script, give a wrong file name. You can see exception!
+    file_handling.write("Modified via Script implemented for Python exceptions!")
+except IOError:
+    print("Error: Can't read the or locate the file/file path")
+else:
+    print("Modifications are applied to this Python file, check the same")
+    file_handling.close()
+print('-----------------------')
+
+
+# file not found error
+
+try:
+    file_handling = open("txt")  # To test this script, give a wrong file name. You can see exception!
+    file_handling.write("Modified via Script implemented for Python exceptions!")
+
+except (IOError, FileNotFoundError):
+    print("Error: Can't read the or locate the file/file path. FileNotFoundError")
+
+else:
+    print("Modifications are applied to this Python file, check the same")
+    file_handling.close()
+print('-----------------------')
